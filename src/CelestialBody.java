504c1c282367757e92479bde14bfe0d35da9d6dc
Wenyue Gu <wenyue.gu@duke.edu>
2019-09-14 22:19:15 -0400

    done

--- src/CelestialBody.java
+++ src/CelestialBody.java
@@ -49,14 +49,28 @@ public class CelestialBody {
 		myFileName = b.getName();
 	}
 
+	/**
+	 * Return the object's value of the x position
+	 * @return value of x position
+	 */
 	public double getX() {
 		// TODO: complete method
 		return myXPos;
 	}
+
+	/**
+	 * Return the body's value of the y position
+	 * @return value of y position
+	 */
 	public double getY() {
 		// TODO: complete method
 		return myYPos;
 	}
+
+	/**
+	 * Return the body's value of the x velocity
+	 * @return value of x velocity
+	 */
 	public double getXVel() {
 		// TODO: complete method
 		return myXVel;
@@ -69,11 +83,18 @@ public class CelestialBody {
 		// TODO: complete method
 		return myYVel;
 	}
-	
+	/**
+	 * Return mass of this Body.
+	 * @return value of mass.
+	 */
 	public double getMass() {
 		// TODO: complete method
 		return myMass;
 	}
+	/**
+	 * Return name of this Body.
+	 * @return value of name.
+	 */
 	public String getName() {
 		// TODO: complete method
 		return myFileName;
@@ -92,22 +113,53 @@ public class CelestialBody {
 		return r;
 	}
 
+	/**
+	 * Return the force between this body and another
+	 * calculated with equation F = Gm1m2/(r^2)
+	 * r is the distance between the two objects, calculated with the calcDistance method
+	 * m1 m2 are the respective mass of the two objects
+	 * @param b the other body
+	 * @return force between this body and b
+	 */
 	public double calcForceExertedBy(CelestialBody b) {
 		double G = 6.67*1e-11;
 		double F = G*myMass*b.getMass()/(this.calcDistance(b)*this.calcDistance(b));
 		// TODO: complete method
 		return F;
 	}
-
+	/**
+	 * Return the force in x direction between this body and another
+	 * calculated with equation Fx = F*dx/r
+	 * r is the distance between the two objects, calculated with the calcDistance method
+	 * dx is the difference between the object's x position
+	 * @param b the other body
+	 * @return force in x direction between this body and b
+	 */
 	public double calcForceExertedByX(CelestialBody b) {
 		// TODO: complete method
 		return this.calcForceExertedBy(b)*(b.getX()-myXPos)/this.calcDistance(b);
 	}
+
+	/**
+	 * Return the force in y direction between this body and another
+	 * calculated with equation Fy = F*dy/r
+	 * r is the distance between the two objects, calculated with the calcDistance method
+	 * dx is the difference between the object's y position
+	 * @param b the other body
+	 * @return force in y direction between this body and b
+	 */
 	public double calcForceExertedByY(CelestialBody b) {
 		// TODO: complete method
 		return this.calcForceExertedBy(b)*(b.getY()-myYPos)/this.calcDistance(b);
 	}
 
+	/**
+	 * Return the net force in x direction between this body and a set of other bodies
+	 * calculate the force between this body and every body in the set, then add
+	 * exclude this body from the set of bodies in case it is in the set
+	 * @param bodies the set of other bodies
+	 * @return net x direction force between this body and the set of body bodies
+	 */
 	public double calcNetForceExertedByX(CelestialBody[] bodies) {
 		// TODO: complete method
 		double net = 0;
@@ -119,7 +171,13 @@ public class CelestialBody {
 		}
 		return net;
 	}
-
+	/**
+	 * Return the net force in y direction between this body and a set of other bodies
+	 * calculate the force between this body and every body in the set, then add
+	 * exclude this body from the set of bodies in case it is in the set
+	 * @param bodies the set of other bodies
+	 * @return net y direction force between this body and the set of body bodies
+	 */
 	public double calcNetForceExertedByY(CelestialBody[] bodies) {
 		// TODO: complete method
 		double net2 = 0;
@@ -131,7 +189,15 @@ public class CelestialBody {
 		}
 		return net2;
 	}
-
+	/**
+	 * Update the x position, y position, x velocity, y velocity of this body
+	 * acceleration is calculated by force/mass
+	 * new velocity is calculated by old velocity + acceleration in this direction * deltaT
+	 * new position is calculated by old position + new velocity in this direction * deltaT
+	 * @param deltaT is the amount of time that has passed since last update
+	 * @param xforce is the force in the x direction
+	 * @param yforce is the force in the y direction
+	 */
 	public void update(double deltaT, 
 			           double xforce, double yforce) {
 		// TODO: complete method
@@ -149,6 +215,9 @@ public class CelestialBody {
 
 	}
 
+	/**
+	 * Draws the star/output the figure
+	 */
 	public void draw() {
 		// TODO: complete method
 		StdDraw.picture(myXPos,myYPos,"images/"+myFileName);
